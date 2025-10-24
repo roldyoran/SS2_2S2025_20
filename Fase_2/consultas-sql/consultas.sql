117d6e94796c125eaadd056940421ef0bcfc7871
roldyoran <edgaralvares4208@gmail.com>
2025-10-24 00:27:56 -0600

    Enhance model evaluation: update SQL queries for tip prediction models and add evaluation images for both versions

--- Fase_2/consultas-sql/consultas.sql
+++ Fase_2/consultas-sql/consultas.sql
@@ -15,42 +15,70 @@ OPTIONS(
 SELECT
   IF(tip_amount > 5, 1, 0) AS tip_five_dolars,
   fare_amount,
-  CAST(payment_type AS INT64) AS payment_type, 
+  trip_distance,
   passenger_count,
+  CAST(payment_type AS INT64) AS payment_type, 
+
+  SAFE_DIVIDE(tip_amount, fare_amount) AS tip_to_fare_ratio,
+
   EXTRACT(DAYOFWEEK FROM pickup_datetime) AS pickup_dayofweek,
 FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022`
 WHERE
     payment_type IN ('1', '2')
     AND tip_amount IS NOT NULL
-    AND fare_amount > 0
     AND tip_amount > 0
+    AND fare_amount > 0
+    AND fare_amount <= 500
     AND passenger_count > 0
-    -- AND EXTRACT(MONTH FROM pickup_datetime) BETWEEN 1 AND 3
-    AND RAND() < 0.1;
+    AND passenger_count <= 6
+    AND trip_distance > 0 
+    AND trip_distance < 1000
+    -- AND RAND() < 0.1
+    AND EXTRACT(MONTH FROM pickup_datetime) BETWEEN 1 AND 6;
+
+
+SELECT * 
+FROM ML.EVALUATE(MODEL `my_dataset.modelo_propinas_v1`);
 
 
 -- MODELO VERSION 2: Modelo de regresión logística para predecir si la propina es mayor a 5 dólares con ligeros cambios en los hiperparámetros
-CREATE OR REPLACE MODEL `ML.modelo_propinas_v2`
+CREATE OR REPLACE MODEL `my_dataset.modelo_propinas_v2`
 OPTIONS(
   model_type = 'logistic_reg',
   input_label_cols = ['tip_five_dolars'],
   data_split_method='RANDOM',
-  data_split_eval_fraction=0.8,
-  l1_reg = 0.1,
+  data_split_eval_fraction=0.25,
+  l1_reg = 0.2,
   l2_reg = 0.05,
-  max_iterations = 15,
+  max_iterations = 20,
   ls_init_learn_rate = 0.2
-) AS    
+) AS
 SELECT
   IF(tip_amount > 5, 1, 0) AS tip_five_dolars,
   fare_amount,
-  CAST(payment_type AS INT64) AS payment_type, 
+  trip_distance,
   passenger_count,
+  CAST(payment_type AS INT64) AS payment_type, 
+
+  SAFE_DIVIDE(tip_amount, fare_amount) AS tip_to_fare_ratio,
+
   EXTRACT(DAYOFWEEK FROM pickup_datetime) AS pickup_dayofweek,
 FROM `bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2022`
 WHERE
-  payment_type IN ('1', '2')
-  AND tip_amount IS NOT NULL
-  AND fare_amount > 0
-  AND tip_amount > 0
-  AND passenger_count > 0;
+    payment_type IN ('1', '2')
+    AND tip_amount IS NOT NULL
+    AND tip_amount > 0
+    AND fare_amount > 0
+    AND fare_amount <= 500
+    AND passenger_count > 0
+    AND passenger_count <= 6
+    AND trip_distance > 0 
+    AND trip_distance < 1000
+    -- AND RAND() < 0.1
+    AND EXTRACT(MONTH FROM pickup_datetime) BETWEEN 1 AND 6;
+
+
+
+
+SELECT * 
+FROM ML.EVALUATE(MODEL `my_dataset.modelo_propinas_v2`);
